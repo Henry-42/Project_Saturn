00634244e17afb70311c8fc8efa69899b52c8b39
Henry <74014339+Henry-42@users.noreply.github.com>
2025-05-14 12:57:07 -0400

    Add files via upload

---
+++ rats.py
@@ -0,0 +1,43 @@
+import mysql.connector
+
+
+def get_db_connect():
+    connection = mysql.connector.connect(user='kyley9',
+                                         password='224085274',
+                                         host='10.8.37.226',
+                                         database='kyley9_db')
+    return connection
+
+
+def execute_query(connection, query):
+    cursor = connection.cursor()
+    cursor.execute(query)
+    results = []
+
+    for row in cursor:
+        results.append(row)
+    cursor.close()
+    connection.close()
+
+    return results
+
+
+def get_student_schedule(sid):
+    statement = "CALL Student_Scehdule_Procedure(" + sid + ")"
+    return execute_query(get_db_connect(), statement)
+
+
+sid = input("Enter Student ID: ")
+
+results = get_student_schedule(sid)
+
+for result in results:
+    period = results[1]
+    course_name = results[2]
+    room = results[3]
+    teacher_name = results[4]
+    print("Period:", period)
+    print("Course:", course_name)
+    print("Room:", room)
+    print("Teacher:", teacher_name)
+    print()
